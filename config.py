84dbd27860443ecf1d5632c83953cc28c32bfe46
EvanCXCheng <24072654@student.uwa.edu.au>
2026-05-11 16:09:50 +0800

    slightly better

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ Network_1 = "10.0.1.0/24"
 Network_2 = "10.0.2.0/24"
 Router_interface_1 = ["10.0.1.1", "BB:BB:BB:BB:BB:BB"]
 Router_interface_2 = ["10.0.2.1", "CC:CC:CC:CC:CC:CC"]
-Host_A = ["10.0.1.10", "AA:AA:AA:AA:AA:AA"]
-Host_A_Port = 5000
-Host_B = ["10.0.2.20", "DD:DD:DD:DD:DD:DD"]
-Host_B_Port = 80
+Host_A = ["10.0.1.10", "AA:AA:AA:AA:AA:AA", 5000]
+Host_B = ["10.0.2.20", "DD:DD:DD:DD:DD:DD", 80]
 Routing_Table = {
 }
