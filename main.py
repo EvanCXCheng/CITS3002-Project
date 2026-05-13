f2ec4285486bcff8ffdfd690b0876bcdac2a5f46
sduproy <seandu2005@gmail.com>
2026-05-13 21:41:28 +0800

    add: makes the objects and calls the methods

--- main.py
+++ main.py
@@ -3,41 +3,20 @@ from config import *
 from protocol import *
 from devices import *
 
-#Can link hardware straight from list in config
-def framing(packet, destination, source): 
-    Type = "0x0800"
-    frame_head = Frame_headers(destination[1], source[1], Type)
-    frame = Frame(frame_head, Packet)
 
 
-def encapsulate(segment, destination, source):
-    TTL = 100
-    Protocol = 17
-    Total_length = segment.segment_head.length + 12
-    packet_head = Packet_header(destination[0], source[0], TTL, Protocol, Total_length)
-    packet = Packet(packet_head, segment)
-    framing(packet)
-
-def segmentation(input_size, destination, source):
-    length = input_size + 10 #Due to header size
-    checksum = "I'll fix this when i learn how checksum works"
-    Type = 0
-    Sequence_number = 0
-    Data = "A" * input_size
-    segment_head = Segment_header(destination[2], source[2], length, checksum, Type, Sequence_number)
-    segment = Segment(segment_head, Data)    
-    print("Host A: Layer 4: Segment created by adding transport layer header (DATA, seq=0) (encapsulation)")
-    print("Host A: Layer 4: Segment sent to Network Layer")
-    encapsulate(segment)
-    pass #?
-
-#on input size, not input data?
-def checksum(input_size):
-    print("Host A: Layer 4: Checksum computed")
-    return segmentation(input_size) #somewhere, somewhere, maybe not hardcode required (can pass which layer came from, decide where to send, map in config?)
+HostA = Host("Host A", Host_A[0], Network_1, Host_A[1], Host_A[2], Host_A_Routing_Table)
+HostB = Host("Host B", Host_B[0], Network_2, Host_B[1], Host_B[2], Host_B_Routing_Table)
+Router1 = Router("Router R1", [Router_interface_1[0], Router_interface_2[0]], [Router_interface_1[1], Router_interface_2[1]], Router_Routing_Table)
 
 
 input_size = int(sys.argv[1])
 print("Host A: Layer 4: Data received from Application Layer. Data size= ",input_size)
 
-new_segment = checksum(input_size)
+frame = HostA.create_segment(input_size, HostB)
+frame = Router1.receive_frame(frame)
+HostB.receive_frame(frame, 0)
+
+ack = HostB.create_ack(1, HostA)
+ack = Router1.receive_frame(ack)
+HostA.receive_frame(ack, 1)
